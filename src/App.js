a98739fe2c62da1dfab17418476780657608e9cf
Akshat Surana <suranaas@rknec.edu>
2023-05-22 12:07:10 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,13 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Page1 from './components/Page1';
 import { Route, Routes } from "react-router-dom";
-
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
 function App() {
 
     return (
-        // <div>
-        //     <Navbar/>
-        //     <div class="container-fluid" id="main">
-        //         <div class="row row-offcanvas row-offcanvas-left">
-        //             <Sidebar />
-        //             <Dashboard />
-        //         </div>
-        //     </div>
-
-
-        // </div>
-        // <Routes>
-        //     <  Route path="/" component={Page1} />
-
-        // </Routes>
-        <Page1/>
-
+        <FirstPage />
+        // <SecondPage/>
 
     );
 }
---
+++ src/pages/FirstPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Page1 from '../components/Page1';
+const FirstPage = () => {
+    return (
+        <Page1 />
+    );
+}
+export default FirstPage
