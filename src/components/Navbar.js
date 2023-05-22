5f276876ec904bbb69a248f314e65170277d4e08
Shristi216 <guptass_7@rknec.edu>
2023-05-22 15:13:29 +0530

    navandside

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 export const Navbar = () => {
-    return (
-        <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-info mb-3">
-            <div className="flex-row d-flex">
-                <button type="button" className="navbar-toggler mr-2 " data-toggle="offcanvas" title="Toggle responsive left sidebar">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a className="navbar-brand" href="#" title="Free Bootstrap 4 Admin Template">Profile page</a>
-            </div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="navbar-collapse collapse" id="collapsingNavbar">
-                <ul className="navbar-nav">
-                    <Link to="/">
-                        <li className="nav-item active">
-                            <a className="nav-link" >First Page<span className="sr-only">first page</span></a>
-                        </li></Link>
-                    {/* <li className="nav-item">
+  return (
+    <nav
+      className="navbar fixed-top navbar-expand-md navbar-dark  mb-3"
+      style={{ backgroundColor: "#35B5AC" }}
+    >
+      <div className="flex-row d-flex">
+        <button
+          type="button"
+          className="navbar-toggler mr-2 "
+          data-toggle="offcanvas"
+          title="Toggle responsive left sidebar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a
+          className="navbar-brand"
+          href="#"
+          title="Free Bootstrap 4 Admin Template"
+        >
+          Profile page
+        </a>
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsingNavbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="navbar-collapse collapse" id="collapsingNavbar">
+        <ul className="navbar-nav">
+          <Link to="/">
+            <li className="nav-item active">
+              <a className="nav-link">
+                First Page<span className="sr-only">first page</span>
+              </a>
+            </li>
+          </Link>
+          {/* <li className="nav-item">
                             <a className="nav-link" href="//www.codeply.com">Link</a>
                         </li> */}
-                </ul>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#myAlert" data-toggle="collapse">Alert</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="" data-target="#myModal" data-toggle="modal">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link waves-effect waves-light text-white">
-                            <i className="fab fa-google-plus-g"></i>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link waves-effect waves-light text-white">
-                            <i className="fas fa-envelope-open-text"></i>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link waves-effect waves-light text-white">
-                            <i className="fas fa-align-justify"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+        </ul>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#myAlert" data-toggle="collapse">
+              Alert
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href=""
+              data-target="#myModal"
+              data-toggle="modal"
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link waves-effect waves-light text-white">
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link waves-effect waves-light text-white">
+              <i className="fas fa-envelope-open-text"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link waves-effect waves-light text-white">
+              <i className="fas fa-align-justify"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,95 @@
-import React from 'react'
+import React from "react";
 const Sidebar = () => {
-    return (
-         <div class="col-md-3 col-lg-2 sidebar-offcanvas pl-0" id="sidebar" role="navigation" style={{backgroundColor:"#2CD3E1"}}>
-            <ul class="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
-                <li class="nav-item mb-2 mt-3"><a class="nav-link text-dark" href="#"><h5>Students name</h5></a></li>
-                <li class="nav-item mb-2 "><a class="nav-link text-dark" href="#"><i class="fas fa-user font-weight-bold"></i> <span className="ml-3">Overview</span></a></li>
-                <li class="nav-item mb-2">
-                    <a class="nav-link text-dark" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="far fa-file-word font-weight-bold"></i> <span className="ml-3"> Reports▾</span></a>
-                    <ul class="list-unstyled flex-column pl-3 collapse" id="submenu1" aria-expanded="false">
-                       <li class="nav-item mb-2 "><a class="nav-link text-dark" href=""><i class="fas fa-book-reader"></i> Data Report </a></li>
-                       <li class="nav-item mb-2 "><a class="nav-link text-dark" href=""> <i class="fas fa-book-medical"></i> File Report </a></li>
-                    </ul>
-                </li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#"><i class="far fa-chart-bar font-weight-bold"></i> <span className="ml-3">Analytics</span></a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#"><i class="fas fa-file-export font-weight-bold"></i><span className="ml-3">Export</span></a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#"><i class="fas fa-tablet-alt font-weight-bold"></i><span className="ml-3">Snippets</span></a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#"><i class="fas fa-atom font-weight-bold"></i> <span className="ml-3">Flex</span></a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#"><i class="far fa-folder font-weight-bold"></i> <span className="ml-3">Layouts</span></a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#">Templates</a></li>
-                <li class="nav-item mb-2"><a class="nav-link text-dark" href="#">Themes</a></li>
-            </ul>
-       </div>
-    )
-}
- 
-export default Sidebar
+  return (
+    <div
+      class="col-md-3 col-lg-2 sidebar-offcanvas pl-0"
+      id="sidebar"
+      role="navigation"
+      style={{ backgroundColor: "#35B5AC" }}
+    >
+      <ul class="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
+        <li class="nav-item mb-2 mt-3">
+          <a class="nav-link text-dark" href="#">
+            <h5>Students name</h5>
+          </a>
+        </li>
+        <li class="nav-item mb-2 ">
+          <a class="nav-link text-dark" href="#">
+            <i class="fas fa-user font-weight-bold"></i>{" "}
+            <span className="ml-3">Overview</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a
+            class="nav-link text-dark"
+            href="#submenu1"
+            data-toggle="collapse"
+            data-target="#submenu1"
+          >
+            <i class="far fa-file-word font-weight-bold"></i>{" "}
+            <span className="ml-3"> Reports▾</span>
+          </a>
+          <ul
+            class="list-unstyled flex-column pl-3 collapse"
+            id="submenu1"
+            aria-expanded="false"
+          >
+            <li class="nav-item mb-2 ">
+              <a class="nav-link text-dark" href="">
+                <i class="fas fa-book-reader"></i> Data Report{" "}
+              </a>
+            </li>
+            <li class="nav-item mb-2 ">
+              <a class="nav-link text-dark" href="">
+                {" "}
+                <i class="fas fa-book-medical"></i> File Report{" "}
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            <i class="far fa-chart-bar font-weight-bold"></i>{" "}
+            <span className="ml-3">Analytics</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            <i class="fas fa-file-export font-weight-bold"></i>
+            <span className="ml-3">Export</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            <i class="fas fa-tablet-alt font-weight-bold"></i>
+            <span className="ml-3">Snippets</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            <i class="fas fa-atom font-weight-bold"></i>{" "}
+            <span className="ml-3">Flex</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            <i class="far fa-folder font-weight-bold"></i>{" "}
+            <span className="ml-3">Layouts</span>
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            Templates
+          </a>
+        </li>
+        <li class="nav-item mb-2">
+          <a class="nav-link text-dark" href="#">
+            Themes
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
