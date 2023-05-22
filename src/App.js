d2df8699c035fd3aac69a2f445f296f448905d0b
Akshat Surana <suranaas@rknec.edu>
2023-05-22 12:08:29 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Page1 from './components/Page1';
 import { Route, Routes } from "react-router-dom";
 import FirstPage from './pages/FirstPage';
 import SecondPage from './pages/SecondPage';
